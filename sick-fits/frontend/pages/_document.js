53d9927e55ee8b8f35a1564e0daa706f5bc0ecc4
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2022-09-02 13:35:58 +0200

    Got TypeScript running

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, NextScript, Main } from "next/document";
 export default class MyDocument extends Document {
     render() {
         return <Html lang="en-CA">
-            {/* <Head></Head> */}
+            <Head></Head>
             <body>
                 <Main />
                 <NextScript />
