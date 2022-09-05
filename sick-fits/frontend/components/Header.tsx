01eb6d124f8b21fb0e9f7f4b3eb0c2dd7e6b8348
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2022-09-05 13:53:24 +0200

    Follow course and try to fix hydration error

--- sick-fits/frontend/components/Header.tsx
+++ sick-fits/frontend/components/Header.tsx
@@ -1,20 +1,53 @@
 import Link from "next/link";
 import Nav from "./Nav";
+import styled from "styled-components";
+
+const HeaderLogo = styled.h1`
+    background: red;
+    font-size: 4rem;
+    margin-left: 2rem;
+    transform: skew(-7deg);
+    z-index: 2;
+    a {
+        color: white;
+        text-decoration: none;
+        text-transform: uppercase;
+        padding: 0.5rem 1rem;
+    }
+`;
+
+const StyledHeader = styled.header`
+    .bar {
+        border-bottom: 10px solid var(--black, black);
+        display: grid;
+        grid-template-columns: auto 1fr;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .sub-bar {
+        border-bottom: 1px solid var(--black, black);
+        display: grid;
+        grid-template-columns: 1fr auto;
+    }
+`
 
 export default function Header() {
     return (
-        <header>
+        <StyledHeader>
             <div className="bar">
-                <Link
-                    href="/"
-                >
-                    Sick fits
-                </Link>
+                <HeaderLogo>
+                    <Link
+                        href="/"
+                    >
+                        Sick fits
+                    </Link>
+                </HeaderLogo>
             </div>
             <Nav />
             <div className="sub-bar">
                 <p>search</p>
             </div>
-        </header>
+        </StyledHeader>
     );
 }
--- sick-fits/frontend/pages/_app.tsx
+++ sick-fits/frontend/pages/_app.tsx
@@ -1,10 +1,19 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import { PageLayout } from "../components/PageLayout";
 
+// TODO: swap with our own
+import "nprogress/nprogress.css";
+import "../components/styles/nprogress.css"
+
+Router.events.on( "routeChangeStart", () => NProgress.start() );
+Router.events.on( "routeChangeComplete", () => NProgress.done() );
+Router.events.on( "routeChangeError", () => NProgress.done() );
+
 export default function MyApp( { Component, pageProps }: AppProps ) {
     return <PageLayout>
-        <h2>This is from _app.tsx</h2>
         <Component { ...pageProps } />
     </PageLayout>
 }
